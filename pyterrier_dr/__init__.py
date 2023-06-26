46d8fc75e5ca72cb7d8249b8c6ffe447862e544a
9meo <sansarn@gmail.com>
2023-06-26 12:06:38 +0100

    Support GTR-T5

--- pyterrier_dr/__init__.py
+++ pyterrier_dr/__init__.py
@@ -1,3 +1,4 @@
 from .indexes import DocnoFile, NilIndex, NumpyIndex, RankedLists, FaissFlat, FaissHnsw, MemIndex, TorchIndex
 from .tctcolbert_model import TctColBert
+from .gtr_model import GTRT5
 from .electra import ElectraScorer
