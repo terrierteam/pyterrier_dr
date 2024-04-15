4c479be1bcf6f1e9839a8e364e41f3fe6758082a
Andreas Chari <andreas9810@outlook.com>
2024-04-15 11:54:26 +0000

    add encoder for bge-m3 embeddings

---
+++ pyterrier_dr/bge_m3.py
@@ -0,0 +1,34 @@
+import torch
+from transformers import AutoConfig
+from .biencoder import BiEncoder
+from tqdm import tqdm
+
+def _BGEM3_encode(self, texts, batch_size=None, max_length=8192, return_dense=True, return_sparse=False, return_colbert_vecs=False):
+    # If you don't need such a long length, you can set a smaller value to speed up the encoding process.
+    # TODO: refactor this to work with BGEM3FlagModel
+    show_progress = False
+    if isinstance(texts, tqdm):
+        texts.disable = True
+        show_progress = True
+    return self.model.encode(list(texts), batch_size=batch_size or self.batch_size, max_length=max_length,
+                             return_dense=return_dense, return_sparse=return_sparse, return_colbert_vecs=return_colbert_vecs)
+
+
+class BGEM3Encoder(BiEncoder):
+    def __init__(self, model_name='BAAI/bge-m3', batch_size=32, text_field='text', verbose=False, device=None):
+        super().__init__(batch_size, text_field, verbose)
+        self.model_name = model_name
+        if device is None:
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = torch.device(device)
+        from FlagEmbedding import BGEM3FlagModel
+        model = BGEM3FlagModel(self.model_name,  
+                       use_fp16=False, device=self.device) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+        self.model = model
+        self.config = AutoConfig.from_pretrained(model_name)
+
+    encode_queries = _BGEM3_encode
+    encode_docs = _BGEM3_encode
+
+    def __repr__(self):
+        return f'BGEM3Encoder({repr(self.model_name)})'
