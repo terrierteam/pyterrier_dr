d711650fd63804a533d998651bae36806737818f
Andreas Chari <andreas9810@outlook.com>
2024-04-15 13:29:50 +0000

    fix return of _BGEM3_encode and return np array directly

--- pyterrier_dr/bge_m3.py
+++ pyterrier_dr/bge_m3.py
@@ -5,13 +5,13 @@ from tqdm import tqdm
 
 def _BGEM3_encode(self, texts, batch_size=None, max_length=8192, return_dense=True, return_sparse=False, return_colbert_vecs=False):
     # If you don't need such a long length, you can set a smaller value to speed up the encoding process.
-    # TODO: refactor this to work with BGEM3FlagModel
     show_progress = False
     if isinstance(texts, tqdm):
         texts.disable = True
-        show_progress = True
-    return self.model.encode(list(texts), batch_size=batch_size or self.batch_size, max_length=max_length,
+    embeddings = self.model.encode(list(texts), batch_size=batch_size or self.batch_size, max_length=max_length,
                              return_dense=return_dense, return_sparse=return_sparse, return_colbert_vecs=return_colbert_vecs)
+    # TODO: add feature to also return sparse and colbert vecs
+    return embeddings['dense_vecs']
 
 
 class BGEM3Encoder(BiEncoder):
@@ -23,7 +23,7 @@ class BGEM3Encoder(BiEncoder):
         self.device = torch.device(device)
         from FlagEmbedding import BGEM3FlagModel
         model = BGEM3FlagModel(self.model_name,  
-                       use_fp16=False, device=self.device) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+                       use_fp16=True, device=self.device) # Setting use_fp16 to True speeds up computation with a slight performance degradation
         self.model = model
         self.config = AutoConfig.from_pretrained(model_name)
 
