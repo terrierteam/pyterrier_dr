2b360e7f0a9feb0e7980260e30f0f706af8f996d
Sean MacAvaney <sean.macavaney@gmail.com>
2024-10-13 16:20:43 +0100

    bump version

--- pyterrier_dr/__init__.py
+++ pyterrier_dr/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .util import SimFn, infer_device
 from .indexes import DocnoFile, NilIndex, NumpyIndex, RankedLists, FaissFlat, FaissHnsw, MemIndex, TorchIndex
