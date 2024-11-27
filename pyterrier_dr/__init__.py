4417edc23de2077a07ca3514fcb55c586165b43c
Sean MacAvaney <sean.macavaney@gmail.com>
2024-11-27 18:27:51 +0000

    fix test

--- pyterrier_dr/__init__.py
+++ pyterrier_dr/__init__.py
@@ -5,7 +5,7 @@ from pyterrier_dr.indexes import DocnoFile, NilIndex, NumpyIndex, RankedLists, F
 from pyterrier_dr.flex import FlexIndex
 from pyterrier_dr.biencoder import BiEncoder, BiQueryEncoder, BiDocEncoder, BiScorer
 from pyterrier_dr.hgf_models import HgfBiEncoder, TasB, RetroMAE
-from pyterrier_dr.sbert_models import SBertBiEncoder, Ance, Query2Query, GTR
+from pyterrier_dr.sbert_models import SBertBiEncoder, Ance, Query2Query, GTR, E5
 from pyterrier_dr.tctcolbert_model import TctColBert
 from pyterrier_dr.electra import ElectraScorer
 from pyterrier_dr.bge_m3 import BGEM3, BGEM3QueryEncoder, BGEM3DocEncoder
@@ -14,5 +14,5 @@ from pyterrier_dr.prf import AveragePrf, VectorPrf
 
 __all__ = ["FlexIndex", "DocnoFile", "NilIndex", "NumpyIndex", "RankedLists", "FaissFlat", "FaissHnsw", "MemIndex", "TorchIndex",
            "BiEncoder", "BiQueryEncoder", "BiDocEncoder", "BiScorer", "HgfBiEncoder", "TasB", "RetroMAE", "SBertBiEncoder", "Ance",
-           "Query2Query", "GTR", "TctColBert", "ElectraScorer", "BGEM3", "BGEM3QueryEncoder", "BGEM3DocEncoder", "CDE", "CDECache",
+           "Query2Query", "GTR", "E5", "TctColBert", "ElectraScorer", "BGEM3", "BGEM3QueryEncoder", "BGEM3DocEncoder", "CDE", "CDECache",
            "SimFn", "infer_device", "AveragePrf", "VectorPrf"]
