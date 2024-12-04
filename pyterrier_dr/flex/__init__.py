31770abaaff3eecc2d762b1501a033bdc7ff2627
Sean MacAvaney <sean.macavaney@gmail.com>
2024-12-04 12:56:03 +0000

    initial implementation

--- pyterrier_dr/flex/__init__.py
+++ pyterrier_dr/flex/__init__.py
@@ -3,9 +3,10 @@ from pyterrier_dr.flex import np_retr
 from pyterrier_dr.flex import torch_retr
 from pyterrier_dr.flex import corpus_graph
 from pyterrier_dr.flex import faiss_retr
+from pyterrier_dr.flex import flatnav_retr
 from pyterrier_dr.flex import scann_retr
 from pyterrier_dr.flex import ladr
 from pyterrier_dr.flex import gar
 from pyterrier_dr.flex import voyager_retr
 
-__all__ = ["FlexIndex", "IndexingMode", "np_retr", "torch_retr", "corpus_graph", "faiss_retr", "scann_retr", "ladr", "gar", "voyager_retr"]
+__all__ = ["FlexIndex", "IndexingMode", "np_retr", "torch_retr", "corpus_graph", "faiss_retr", "flatnav_retr", "scann_retr", "ladr", "gar", "voyager_retr"]
