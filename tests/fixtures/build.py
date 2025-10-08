2e60be207d9289f75fd8005fb2aa0aa580fb233f
Nicola Tonellotto <nicola.tonellotto@gmail.com>
2025-10-08 06:13:53 +0000

    fixed device selection in build.py

--- tests/fixtures/build.py
+++ tests/fixtures/build.py
@@ -1,5 +1,15 @@
 import pyterrier_dr, torch, pyterrier as pt
-tct = pyterrier_dr.TctColBert(device=torch.device("mps"))
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+print(f"Using device: {device}")
+
+tct = pyterrier_dr.TctColBert(device=device)
 index = pyterrier_dr.FlexIndex("./vaswani_tct.flex")
 (tct >> index).index(pt.get_dataset("vaswani").get_corpus_iter())
 print(len(index))
