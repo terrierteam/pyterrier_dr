3bbc1abff9d3adfd82741cd3070b8e01a5fc7cb9
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2025-10-14 11:51:53 +0100

    add retrieval evals into training loop, probably temporarily

--- pyterrier_dr/jpq/retriever.py
+++ pyterrier_dr/jpq/retriever.py
@@ -2,10 +2,11 @@ from typing import List, Optional
 import pandas as pd
 import numpy as np
 import pyterrier as pt
+from pyterrier_dr import FlexIndex
+from pyterrier_dr.jpq.pq import ProductQuantizer
 import torch
 from .utils import timer
 from pyterrier import tqdm
-import faiss
 
 # class JPQRetrieverFlatEncoder(pt.Transformer):
 #     """Subset-mode retriever (Flat-IP reconstructed from codes)."""
@@ -52,7 +53,31 @@ import faiss
 #             for r, (did, s) in enumerate(zip(I[i], D[i]), start=1):
 #                 rows.append((qid, self.docnos[int(did)], float(s), r))
 #         return pd.DataFrame(rows, columns=['qid','docno','score','rank'])
-    
+
+
+def build_from_flex(existing_index : FlexIndex, pq : ProductQuantizer, biencoder, dest_folder : str, bs : int = 20_000) -> FlexIndex:
+
+        docnos, original_vec, _ = existing_index.payload()
+        pm = biencoder.passage.to("cpu").eval()
+        
+        def _gen():
+            running_se = 0.
+            with torch.no_grad():
+                for i in tqdm(range(0, len(existing_index), bs), desc=f"build flat", leave=False):
+                    codes_batch = pq.encode(original_vec[i:i+bs])
+                    embs = pm(torch.Tensor(codes_batch).int())
+                    embs = (embs / (embs.norm(dim=1, keepdim=True) + 1e-12)).detach().cpu().numpy().astype('float32')
+                    for j in range(codes_batch.shape[0]):
+                        running_se += np.sum((embs[j] - original_vec[i+j])**2)
+                        yield {'docno' : docnos[i+j], 'doc_vec' : embs[j]}
+                print("Emb MSE", running_se/len(existing_index))
+        
+        new_index = FlexIndex(dest_folder).indexer(mode='overwrite').index(_gen())
+        
+        print("Old index size in docs", len(existing_index))
+        print("New index size in docs", len(new_index))
+        
+        return new_index
 
 class JPQRetrieverFlat(pt.Transformer):
     """Subset-mode retriever (Flat-IP reconstructed from codes)."""
@@ -72,6 +97,7 @@ class JPQRetrieverFlat(pt.Transformer):
     def _ensure(self, bs: int = 20000):
         if self._index is not None: return
         dim = self.biencoder.passage.sub_embeddings[0].embedding_dim * self.biencoder.passage.M
+        import faiss
         index = faiss.IndexFlatIP(dim)
         pm = self.biencoder.passage.to("cpu").eval()
         with torch.no_grad():
