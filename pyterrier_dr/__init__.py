48464b57f3ef690418719db811de4a755c40b5f3
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2025-11-19 08:06:43 +0000

    dont import lion by default

--- pyterrier_dr/__init__.py
+++ pyterrier_dr/__init__.py
@@ -9,6 +9,8 @@ from pyterrier_dr.sbert_models import SBertBiEncoder, Ance, Query2Query, GTR, E5
 from pyterrier_dr.tctcolbert_model import TctColBert
 from pyterrier_dr.electra import ElectraScorer
 from pyterrier_dr.lightning_ir_mono import LightningIRMonoScorer
+from pyterrier_dr.star import STAR
+#from pyterrier_dr.lion import LionLlamaDense
 from pyterrier_dr.bge_m3 import BGEM3, BGEM3QueryEncoder, BGEM3DocEncoder
 from pyterrier_dr.cde import CDE, CDECache
 from pyterrier_dr.prf import AveragePrf, VectorPrf
@@ -18,4 +20,5 @@ from pyterrier_dr._mmr import MmrScorer
 __all__ = ["FlexIndex", "DocnoFile", "NilIndex", "NumpyIndex", "RankedLists", "FaissFlat", "FaissHnsw", "MemIndex", "TorchIndex",
            "BiEncoder", "BiQueryEncoder", "BiDocEncoder", "BiScorer", "HgfBiEncoder", "TasB", "RetroMAE", "SBertBiEncoder", "Ance",
            "Query2Query", "GTR", "E5", "TctColBert", "ElectraScorer", "LightningIRMonoScorer", "BGEM3", "BGEM3QueryEncoder", "BGEM3DocEncoder", "CDE", "CDECache",
+           "STAR", #"LionLlamaDense",
            "SimFn", "infer_device", "AveragePrf", "VectorPrf", "ILS", "ils", "MmrScorer"]
