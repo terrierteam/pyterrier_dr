1646ad10f89685c582a4e8682a0d75e97ab5145e
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2025-11-20 20:57:29 +0000

    add xformers ; fix new model

--- pyterrier_dr/repllama.py
+++ pyterrier_dr/repllama.py
@@ -6,6 +6,7 @@ from .biencoder import BiEncoder
 from .util import Variants
 
 def _get_model(peft_model_name):
+    #replace_with_xformers_attention()
     from peft import PeftModel, PeftConfig
     config = PeftConfig.from_pretrained(peft_model_name)
     base_model = AutoModel.from_pretrained(config.base_model_name_or_path)
@@ -13,6 +14,60 @@ def _get_model(peft_model_name):
     model = model.merge_and_unload()
     model.eval()
     return model
+
+def replace_with_xformers_attention():
+    import torch
+    import xformers.ops as xops
+
+    from typing import Optional, Tuple
+    from transformers.models.llama.modeling_llama import LlamaAttention, apply_rotary_pos_emb
+
+    def custom_forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+        kv_seq_len = key_states.shape[-2]
+        if past_key_value is not None:
+            kv_seq_len += past_key_value[0].shape[-2]
+        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+
+        if past_key_value is not None:
+            # reuse k, v, self_attention
+            key_states = torch.cat([past_key_value[0], key_states], dim=2)
+            value_states = torch.cat([past_key_value[1], value_states], dim=2)
+
+        past_key_value = (key_states, value_states) if use_cache else None
+
+        attn_weights = None
+        attn_output = xops.memory_efficient_attention(
+            query_states.transpose(1, 2), key_states.transpose(1, 2), value_states.transpose(1, 2),
+            attn_bias=xops.LowerTriangularMask()
+        ).reshape(bsz, q_len, self.hidden_size)
+
+        attn_output = self.o_proj(attn_output)
+
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights, past_key_value
+
+    LlamaAttention.forward = custom_forward
 
 class _RepLLamaBiEncoderBase(BiEncoder):
     def __init__(self, model: str, tokenizer, batch_size=32, text_field='text', verbose=False, device=None):
@@ -25,6 +80,8 @@ class _RepLLamaBiEncoderBase(BiEncoder):
         tokenizer.pad_token = tokenizer.unk_token
         tokenizer.padding_side = "right"
         self.tokenizer = tokenizer
+
+    # padding handling as per https://github.com/texttron/tevatron/blob/main/examples/repllama/repllama.py
 
     def encode_queries_torch(self, texts, batch_size=None):
         results = []
@@ -51,7 +108,7 @@ class _RepLLamaBiEncoderBase(BiEncoder):
                 # NB: titles should be prepended in the indexing pipeline, if available
                 inps = self.tokenizer([f'passage: {passage}</s>' for passage in chunk], return_tensors='pt', padding=True, truncation=True, max_length=256)
                 inps = {k: v.to(self.device) for k, v in inps.items()}
-                psg_out = self.lm_p(**inps, output_hidden_states=True)
+                psg_out = self.model(**inps, output_hidden_states=True)
                 p_hidden = psg_out.hidden_states[-1]
                 attention_mask = inps['attention_mask']
                 # we want the last token representation that is not padding
