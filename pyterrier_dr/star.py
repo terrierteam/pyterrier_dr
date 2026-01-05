71b862be23fdaca22a76ccbed17398e3f71829f7
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2026-01-05 10:45:35 +0000

    various star changes

--- pyterrier_dr/star.py
+++ pyterrier_dr/star.py
@@ -27,13 +27,15 @@ class STAR(BiEncoder):
         super().__init__(batch_size=batch_size, verbose=verbose, text_field=text_field)
         self.device = device or torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         self.model = RobertaDot.from_pretrained(model_name).to(self.device).eval()
+
+        # uses a copy of the old tokenizer code, and therefore the old hgf API
         from ._star_tokenizer import RobertaTokenizer
-        self.tokenizer = RobertaTokenizer.from_pretrained(model_name)
+        self.tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 
     def encode_queries_torch(self, texts, batch_size=None):
         results = []
         for chunk in chunked(texts, batch_size or self.batch_size):
-            inps = self.tokenizer(list(chunk),  max_length=192, return_tensors='pt', padding="longest", truncation=True)
+            inps = self.tokenizer.batch_encode_plus(list(chunk),  max_length=192, return_tensors='pt', pad_to_max_length=True, truncation=True)
             inps = {k: v.to(self.device) for k, v in inps.items()}
             res = self.model.forward(**inps)
             results.append(res)
@@ -45,7 +47,7 @@ class STAR(BiEncoder):
         results = []
         with torch.no_grad():
             for chunk in chunked(texts, batch_size or self.batch_size):
-                inps = self.tokenizer(list(chunk), max_length=192, return_tensors='pt', padding='longest', truncation=True)
+                inps = self.tokenizer.batch_encode_plus(list(chunk), max_length=192, return_tensors='pt', pad_to_max_length=True, truncation=True)
                 inps = {k: v.to(self.device) for k, v in inps.items()}
                 res = self.model.forward(**inps)
                 results.append(res.cpu().numpy())
